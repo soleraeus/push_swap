b28c974a0bbe32ac83f6518cafc937ff9afd2fca
Soleraeus <soleraeus@gmail.com>
2022-01-21 09:45:00 +0100

    max2 is 5633

--- bash-tester.sh
+++ bash-tester.sh
@@ -2,8 +2,10 @@
 x=1
 max=0
 maxargs=0
+min=10000
+minargs=0
 make
-while [ $x -le 20000 ]
+while [ $x -le 10000 ]
 do
 	ARG=`ruby -e "puts (1..500).to_a.shuffle.join(' ')"`
 	result=$(./push_swap $ARG | wc -l)
@@ -12,7 +14,14 @@ do
 		max=$result
 		maxargs=$ARG
 	fi
+	if [ $result -lt $min ]
+	then
+		min=$result
+		minargs=$ARG
+	fi
 	x=$(( $x + 1 ))
 done
-echo "max number: $max" > results.txt
-echo "$maxargs" > args.txt
+echo "max number: $max"
+echo "$maxargs" > maxargs2.txt
+echo "min number: $min"
+echo "$minargs" > minargs2.txt
